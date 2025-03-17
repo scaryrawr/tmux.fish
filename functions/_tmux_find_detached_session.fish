48350ffef21782aaaf2bb77b2815fb3c5e0cd997
Mike Wallio <michael.wallio@outlook.com>
2025-03-17 16:22:32 -0400

    Fix finding detached sessions

--- functions/_tmux_find_detached_session.fish
+++ functions/_tmux_find_detached_session.fish
@@ -1,12 +1,5 @@
 function _tmux_find_detached_session
     # List sessions and find one that's detached
     set -f term_id (_tmux_get_term_id)
-    for session in (tmux list-sessions 2>/dev/null | grep "$term_id")
-        # Extract session name (part before the colon)
-        set -l session_name (echo $session | cut -d: -f1)
-        # Check if session is detached
-        if echo $session | grep -v '(attached)'
-            echo $session_name
-        end
-    end
+    tmux list-sessions 2>/dev/null | grep "$term_id" | grep -v '(attached)' | cut -d: -f1
 end
--- functions/_tmux_get_session_name.fish
+++ functions/_tmux_get_session_name.fish
@@ -4,7 +4,7 @@ function _tmux_get_session_name
 
     # If we have existing sessions, use the first one
     if test -n "$existing_sessions"
-        set -f tmux_session_name $existing_sessions[0]
+        set -f tmux_session_name $existing_sessions[1]
     else
         # Create a new session with a unique name based on the terminal and timestamp
         set -f stamp (date +'%s')
