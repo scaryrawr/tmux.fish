24f70ed0e2694376b75d6d33ea57bdd537c5cc3d
Mike Wallio <michael.wallio@outlook.com>
2025-03-29 21:38:29 -0400

    VS Code sessions based on PWD

--- functions/_tmux_get_term_id.fish
+++ functions/_tmux_get_term_id.fish
@@ -1,7 +1,10 @@
 function _tmux_get_term_id
-    if test -n "$TERM_PROGRAM"
-      echo $TERM_PROGRAM
+    if test "$TERM_PROGRAM" = vscode -o "$VSCODE_INJECTION" = 1
+        set dir (basename $PWD | string replace -a '.' '_')
+        echo "$dir"
+    else if test -n "$TERM_PROGRAM"
+        echo $TERM_PROGRAM
     else if test -n "$TERM"
-      echo $TERM
+        echo $TERM
     end
 end
