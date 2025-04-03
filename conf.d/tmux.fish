8170780733ea8e3b270c8e41164563ffd0378906
Mike Wallio <michael.wallio@outlook.com>
2025-04-03 15:53:44 -0400

    Simplify environment loading
    Remove environment saving

--- conf.d/tmux.fish
+++ conf.d/tmux.fish
@@ -22,16 +22,4 @@ if not tmux has-session -t "$tmux_session_name" 2>/dev/null
     tmux new-session -d -s "$tmux_session_name"
 end
 
-# VS Code has some special environment variables that need to be set to use code command in tmux in codespaces
-if test "$TERM_PROGRAM" = vscode
-    for line in (env | grep VSCODE)
-        set -l name (echo $line | cut -d= -f1)
-        set -l value (echo $line | cut -d= -f2-)
-        tmux setenv -t "$tmux_session_name" $name "$value"
-    end
-
-    # In codespaces, the PATH has a specific path to code command for the server
-    tmux setenv -t "$tmux_session_name" PATH "$PATH"
-end
-
 tmux attach-session -t "$tmux_session_name" 1>/dev/null && kill $fish_pid
--- functions/_tmux_load_env.fish
+++ functions/_tmux_load_env.fish
@@ -1,4 +1,4 @@
 function _tmux_load_env
-  set -l sh_env (tmux showenv -st (tmux display-message -p '#S'))
+  set -l sh_env (tmux showenv -s)
   replay "$sh_env"
 end
